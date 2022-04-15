011ac18a579840409669909223382853716c6793
deyaa97 <45268366+deyaa97@users.noreply.github.com>
2022-04-15 11:56:21 +0000

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -1,14 +1,23 @@
 # code starts
 alpha = 'abcdefghijklmnopqrstuvwxyz'
+num = '0123456789'
 final = ''
-myinput = input("Enter a String")
-key = int(input("Enter Value of Key"))
+
+myinput = input("Enter a String\n")
+key = int(input("Enter Value of Key\n"))
 myinput = myinput.lower()
+
 for x in myinput:
     if(x in alpha):
         pos = alpha.find(x)
         pos = (pos+key)%26
         final+=(alpha[pos])
+    # add function for numbers
+    elif (x in num):
+        pos = num.find(x)
+        pos = (pos+key)%10
+        final+= (num[pos])
+        
     else:
         final+=x
 
