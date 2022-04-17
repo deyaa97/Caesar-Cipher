31b5c97bd97b252a3b0639e60b6fde8c0030f64c
deyaa97 <45268366+deyaa97@users.noreply.github.com>
2022-04-17 15:48:29 +0000

    Create Polybius.py
    Implement the Polybius cypher

---
+++ Polybius.py
@@ -0,0 +1,36 @@
+# Python Program to implement polybius cipher
+  
+# function to display polybius cipher text
+def polybiusCipher(s):
+  
+        # convert each character to its encrypted code
+        for char in s:
+              
+                # finding row of the table
+                row = int((ord(char) - ord('a')) / 5) + 1
+          
+                # finding column of the table 
+                col = ((ord(char) - ord('a')) % 5) + 1
+  
+                # if character is 'k'
+                if char == 'k':
+                        row = row - 1
+                        col = 5 - col + 1
+                          
+                # if character is greater than 'j'
+                elif ord(char) >= ord('j'):
+                        if col == 1 :
+                            col = 6
+                            row = row - 1
+                              
+                        col = col - 1
+                          
+                print(row, col, end ='', sep ='')
+  
+# Driver's Code
+if __name__ == "__main__":
+  
+        s = "testtext"
+  
+        # print the cipher of "geeksforgeeks"
+        polybiusCipher(s)
